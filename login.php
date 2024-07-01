0926b78e4f2d1a854a78a1922cea104bc1955af7
Dario Miranda <netobis2015@gmail.com>
2024-07-01 17:30:30 -0300

    create file valida login + login, mas ta com erro pois o botao cadastra ao inves de validar

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/cadastro-usuario.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login teu</h2>
+        <form class="container-form" action="processa-cadastro.php" method="POST">
+            <div class="container-inputs">
+                <div class="form-group">
+                    <label for="email">E-mail:</label>
+                    <input class="txt-area" type="email" id="email" name="email" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="senha">Senha:</label>
+                    <input class="txt-area" type="password" id="senha" name="senha" required>
+                </div>
+            </div>
+            
+            <div class="btn-container">
+                <div class="form-group">
+                    <button class="btn-login" type="submit">Login</button>
+                </div>
+                <div class="form-group">
+                    <a href="index.php"><button class="btn-voltar" type="button">Voltar</button></a>
+                </div>
+            </div>
+            
+        </form>
+    </div>
+    
+</body>
+</html>
---
+++ valida-login.php
@@ -0,0 +1,35 @@
+<?php
+
+try{
+
+include 'conexao.php';
+
+$email = "teste1@gmail.com";
+$senha = "111111";
+
+
+$sql = "SELECT COUNT(email) as total FROM tb_usuario WHERE email='$email' AND BINARY senha='$senha'";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute();
+
+$dados = $stmt->fetchAll((PDO::FETCH_ASSOC));
+
+
+if($dados[0]['total'] == 0){
+    // pagina de erro 
+    header('Location: erro.php');
+}else{
+    // pagina de sucesso
+   header('Location: pagina-interna.php');
+}
+
+}catch(PDOException $err){
+    echo "Erro".$err->getMessage();
+}
+
+
+
+
+?>
