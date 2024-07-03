56b1f1a9173651650b38670a68c16f9cad5deed9
Dario Miranda <netobis2015@gmail.com>
2024-07-03 20:20:06 -0300

    update error

--- valida-login.php
+++ valida-login.php
@@ -2,32 +2,32 @@
 
 try{
 
-include 'conexao.php';
+    include 'conexao.php';
 
-$email = "teste1@gmail.com";
-$senha = "111111";
-
-
-$sql = "SELECT COUNT(email) as total FROM tb_usuario WHERE email='$email' AND BINARY senha='$senha'";
-
-$stmt = $conn->prepare($sql);
-
-$stmt->execute();
-
-$dados = $stmt->fetchAll((PDO::FETCH_ASSOC));
+    $email = "teste1@gmail.com";
+    $senha = "111111";
 
 
-if($dados[0]['total'] == 0){
-    // pagina de erro 
-    header('Location: erro.php');
-}else{
-    // pagina de sucesso
-   header('Location: pagina-interna.php');
-}
+    $sql = "SELECT COUNT(email) as total FROM tb_usuario WHERE email='$email' AND BINARY senha='$senha'";
 
-}catch(PDOException $err){
-    echo "Erro".$err->getMessage();
-}
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+
+    $dados = $stmt->fetchAll((PDO::FETCH_ASSOC)); //senha dando erro no fetch e fetchAll
+
+
+    if($dados[0]['total'] == 0){
+        // pagina de erro 
+        header('Location: erro.php');
+    }else{
+        // pagina de sucesso
+    header('Location: pagina-interna.php');
+    }
+
+    }catch(PDOException $err){
+        echo "Erro".$err->getMessage();
+    }
 
 
 
